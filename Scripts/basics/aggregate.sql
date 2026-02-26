31da3f354e82b1dba05362e13d35917cedcb9ed5
Diego Sol <dmsol@sandia.gov>
2026-02-26 14:07:01 -0700

    Complete aggregate example

--- Scripts/basics/aggregate.sql
+++ Scripts/basics/aggregate.sql
@@ -1,5 +1,5 @@
 -- Run CLI
--- sqlite3 -header -box nullvalue NULL jobs_2023.sqlite < Scripts/basics/aggregate.sql > Outputs/basics/aggregate_output.txt
+-- sqlite3 -header -box -nullvalue NULL jobs_2023.sqlite < Scripts/basics/aggregate.sql > Outputs/basics/aggregate_output.txt
 
 SELECT
   SUM(salary_year_avg) AS salary_sum,
@@ -19,6 +19,7 @@ WHERE
 
 SELECT
   job_title_short AS job_title,
+  COUNT(job_title_short) as job_count,
   AVG(salary_year_avg) AS salary_avg,
   MIN(salary_year_avg) AS salary_min,
   MAX(salary_year_avg) AS salary_max
@@ -26,3 +27,7 @@ FROM
   job_postings_fact
 GROUP BY
   job_title_short
+HAVING
+  job_count > 100
+ORDER BY
+  salary_avg DESC
